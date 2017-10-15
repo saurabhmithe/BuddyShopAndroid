02df82ad582a0f73c5d69b0468aa2f20d3192bff
Saurabh <saurabh@Saurabhs-MacBook-Pro.local>
2017-10-14 18:30:44 -0700

    adding splash and login flows

---
+++ app/src/main/java/com/teqsquash/buddyshop/SplashActivity.java
@@ -0,0 +1,27 @@
+package com.teqsquash.buddyshop;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.os.Handler;
+import android.support.v7.app.AppCompatActivity;
+
+public class SplashActivity extends AppCompatActivity {
+
+    private static final int DELAY = 2000;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_splash);
+
+        new Handler().postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                // Create an Intent that will start the LoginActivity after the specified delay.
+                Intent mainIntent = new Intent(SplashActivity.this, LoginActivity.class);
+                SplashActivity.this.startActivity(mainIntent);
+                SplashActivity.this.finish();
+            }
+        }, DELAY);
+    }
+}
